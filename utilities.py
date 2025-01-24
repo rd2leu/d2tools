a2589f345095cddad50673eeaabd5ca0e2963ea3
jeromehage <jeromehage>
2025-01-24 15:13:18 +0100

    restructure utilities

--- utilities.py
+++ utilities.py
@@ -1,62 +1,19 @@
 # src/utilities.py
 
-import pandas as pd
-import numpy as np
+def rank2mmr(rank, immortal = None):
+    """
+    convert rank_tier to approximate mmr
 
-def extract_account_id(profile_url, index = 0, kwd = 'players/'):
-    try:
-        splt = profile_url[index:].split(kwd)
-        a = splt[1]
-        for i, c in enumerate(a):
-            if not c.isdigit():
-                a = a[:i]
-                break
-        return a, index + len(kwd) + len(splt[0]) + i + 1 # FIXME needs testing
-    except:
-        raise SyntaxError('Invalid account url: ' + profile_url)
-
-def extract_account_id2(profile_url):
-    try:
-        return extract_account_id(profile_url)[0]
-    except:
-        return 105248644 # Miracle-
-
-def extract_account_ids(profile_url):
-    # euh idk why
-    try:
-        acc, idx = extract_account_id(profile_url)
-    except:
-        acc, idx = '', 0
-    if acc == '':
-        return []
-    return [acc] + extract_account_ids(profile_url[idx:])
-
-def extract_account_ids2(profile_url):
-    # euh idk why
-    acc, idx = '', 0
-    for k in ['players/', 'player/']:
-        try:
-            acc, idx = extract_account_id(profile_url, kwd = k)
-            break
-        except:
-            pass
-    if acc == '':
-        return []
-    return [acc] + extract_account_ids2(profile_url[idx:])
-
-def shorttime(s):
-    """seconds to short time string"""
-    frmt = '%#Hh%#Mm%#Ss'
-    if s < 3600:
-        frmt = '%#Mm%#Ss'
-    return pd.to_datetime([s], unit = 's').strftime(frmt).values[0]
-
-def datestr(s, timezone = 'CET', frmt = None):
-    """seconds to date string"""
-    if frmt is None:
-        frmt = '%B %d, %Y - %H:%M'
-    return pd.to_datetime(s, unit = 's').tz_localize('UTC').tz_convert(timezone).strftime(frmt)
-
-def datetoseconds(datestr, timezone = 'CET'):
-    """date string to seconds"""
-    return int(pd.to_datetime([datestr]).tz_localize(timezone).tz_convert('UTC').values[0].astype(np.uint64) / 1000000000)
+    param rank rank_tier from opendota
+    param immortal return value when medal is immortal and above
+    return approximate mmr
+    """
+    if rank == None:
+        return 0
+    if rank >= 80:
+        return immortal
+    mmrm = [10, 770, 1540, 2310, 3080, 3850, 4620, 5630]
+    medal = (rank // 10 - 1)
+    mmr = mmrm[medal]
+    mmr += (mmrm[medal + 1] - mmrm[medal]) * ((((rank % 10) - 1) / 5) + 0.1)
+    return mmr

122dd45d2c2b7d84bbb465d14f043eb372fd27b5
jeromehage <jeromehage>
2025-01-24 15:14:21 +0100

    resolve custom steam ID + steam API get_matches

--- api.py
+++ api.py
@@ -3,31 +3,13 @@
 import json, time
 import numpy as np
 
+from .utilities import rank2mmr
 from .network import cached_requests, stored_requests
 #cache = cached_requests()
 cache = stored_requests()
 
-_opendota_delay = 4
+_opendota_delay = 1
 _steamweb_delay = 0
-
-## opendota utilities
-def rank2mmr(rank, immortal = None):
-    """
-    convert rank_tier to approximate mmr
-
-    param rank rank_tier from opendota
-    param immortal return value when medal is immortal and above
-    return approximate mmr
-    """
-    if rank == None:
-        return 0
-    if rank >= 80:
-        return immortal
-    mmrm = [10, 770, 1540, 2310, 3080, 3850, 4620, 5630]
-    medal = (rank // 10 - 1)
-    mmr = mmrm[medal]
-    mmr += (mmrm[medal + 1] - mmrm[medal]) * ((((rank % 10) - 1) / 5) + 0.1)
-    return mmr
 
 ## opendota api
 def _opendota_get(method, force, **parameters):
@@ -37,7 +19,7 @@ def _opendota_get(method, force, **parameters):
         params = '?' + '&'.join('{}={}'.format(k, v) for k, v in parameters.items())
     data, cached = cache.request(url.format(method, params), force)
     if not cached:
-        time.sleep(_opendota_delay)
+        time.sleep(_opendota_delay + np.random.rand())
     return data.json()
 
 def get_player_data(account_id, force = False):
@@ -90,21 +72,48 @@ def _steam_get(method, force, **parameters):
         time.sleep(_steamweb_delay)
     return data.json()
 
+def resolve_id(custom_id, force = False):
+    """resolve steam custom ID"""
+    url = 'ISteamUser/ResolveVanityURL'
+    res = _steam_get(url, force, vanityurl = custom_id)['response']
+    return res['steamid']
+
 def get_matches(force = False, **parameters):
     """league_id, hero_id, game_mode... see steamwebapi.azurewebsites.net for options"""
     url = 'IDOTA2Match_570/GetMatchHistory'
     return _steam_get(url, force, **parameters)
 
 def get_league_matches(league_id, force = False, **parameters):
-    matches = [get_matches(force, league_id = league_id, **parameters)]
-    while matches[-1]['result']['results_remaining']:
-        match_id = matches[-1]['result']['matches'][-1]['match_id']
-        matches += [get_matches(force, league_id = league_id, start_at_match_id = match_id, **parameters)]
-    matches = [m for mm in matches for m in mm['result']['matches']]
+    matches = []
+    # do while
+    request = get_matches(force, league_id = league_id, **parameters)
+    matches.extend(request['result']['matches'])
+    
+    remaining = request['result']['results_remaining']
+    match_ids = [m['match_id'] for m in request['result']['matches']]
+    last_match_id = sorted(match_ids)[0] # they are sorted, but just in case
+
+    parameters.pop('start_at_match_id', None) # after the first request, no longer pass this argument
+
+    while remaining > 0:
+        request = get_matches(
+            force, league_id = league_id,
+            start_at_match_id = last_match_id,
+            **parameters
+            )
+        matches.extend(request['result']['matches'])
+
+        remaining = request['result']['results_remaining']
+        match_ids = [m['match_id'] for m in request['result']['matches']]
+        last_match_id = sorted(match_ids)[0] # they are sorted, but just in case
+        
+    # find duplicates
     matches = np.array(matches)
+    match_ids = [m['match_id'] for m in matches]
+    u, c = np.unique(match_ids, return_counts = True)
+    # sort u from most recent to oldest match id
+    order = np.argsort(u)[::-1]
+    u, c = u[order], c[order]
     # drop duplicates
-    m_ids = [m['match_id'] for m in matches]
-    u, c = np.unique(m_ids, return_counts = True)
-    # duplicates = matches[np.isin(m_ids, u[np.where(c == c[np.argmax(c)])])]
-    first = matches[np.searchsorted(np.sort(m_ids), u)]
+    first = matches[[match_ids.index(m) for m in u]]
     return first.tolist()
